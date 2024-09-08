3980ae5d5d32bc2f9d584185e0d97252365fca3a
s2yedzohaib <s2yedzohaib@gmail.com>
2024-09-08 04:01:43 -0700

    Complete

---
+++ app.js
@@ -0,0 +1,218 @@
+// chp 1 (Alert)
+
+// Q1
+
+// alert('Hello, User!');
+
+// Q1
+
+// Q2
+
+// alert('Error! Please Enter Again');
+
+// Q2
+// Q3
+// alert("Welcome To js \n Happy Coding")
+// Q3
+// Q4
+
+// alert('Welcome to js land');
+// alert('Happy coding \n prevent page from creating additionaly dialogs.');
+// Q4
+// Q5
+// alert("Hello.. i can run browser console")
+// Q5
+        
+// Q6
+//  alert("Hello world") 
+// Q6
+
+
+
+
+// chp 2 (variable for string)
+
+// q1
+// var username;
+// q1
+// q2
+
+// var myName = "syed zohaib shehzad"
+
+// q2
+// q3
+
+// var message = "Hello World"
+// alert(message)
+
+// q3
+// q4
+// alert("Zohaib")
+// alert("15year")
+// alert("Certified")
+// q4
+// q5
+// alert("pizza")
+// alert("pizz")
+// alert("piz")
+// alert("pi")
+// alert("p")
+// q5
+// q6
+// var email = "example@example.com"
+// alert(email)
+// q6
+// q7
+// var book = "A smarter way to learn JavaScript"
+// alert(book)
+// q7
+// q8
+// document.write("Oh yeah  olka")
+
+// q8 
+// q9
+// var son = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(son)
+// q9
+
+
+// chp 3 (variable for number)
+
+// Q1
+// var age = " i am 15 year old "
+
+// alert(age)
+
+
+// Q1
+// Q2
+// var visitedNumOfUser = 'You have visited this site 14 times'
+// alert(visitedNumOfUser)
+// Q2
+// Q3
+// var birthYear = 2010
+// document.write(birthYear)
+
+// Q3
+// Q4
+// var visitorName = "John Doe";
+// var productTitle = "T-shirt"; 
+// var quantity = 5; 
+
+// alert(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store.");
+
+// Q4
+
+// chp 4 (VARIABLE NAMES: LEGAL & ILLEGAL )
+
+// Q1
+// Corrected declaration of three variables in one statement
+// var names = "Zohaib", father = "Shehzad", age = 14;
+
+// Displaying the concatenated result
+// document.write(names + father + age);
+
+// Q1
+// q2
+// legal
+// var down_op = "op"
+// var downOp = "op"
+// var down$op = "op"
+// var down$op$ = "op"
+// var do$wn$op$ = "op"
+// iligal
+
+// var downO#p = "op"
+// var down!$op = "op"
+// var dow@n$op$ = "op"
+// var do$w(n$op$ = "op"
+// var dw(n$o$ = "op"
+// q2
+// Q3
+
+
+
+// let heading = "Rules for naming JS variables <br>";
+// let rule1 = "<br>Variable names can only contain letters, numbers, $, and _. For example: $my_1stVariable";
+// let rule2 = "br>Variables must begin with a letter, $, or _. For example: $name, _name, or name";
+// let rule3 = "Variable names are case sensitive.";
+// let rule4 = "<br>Variable names should not be JS keywords.";
+
+// document.write(heading);
+// document.write(rule1);
+// document.write(rule2);
+// document.write(rule3);
+// document.write(rule4);
+
+
+// Q3
+
+
+
+// chapter 1-3 docx 
+// chp 1 (alert)
+
+// q1
+// alert("zohaib")
+// alert("shehzad")
+// alert("zohaibshehzad@gmail.com")
+// alert("0202020202")
+// alert("123456789")
+// q1
+// q2
+// alert("Youre learning JavaScript!")
+// q2
+// q3
+// alert("I am Syed Zohaib shehzad And A Young Full Stack Developer... Agency(Talha)");
+// q3
+
+// Chapter 2 (Variables for string)
+
+// q1
+// let myVariable;
+
+// q1
+// q2
+// let undefinedVariable;
+// undefinedVariable = "This is now defined.";
+// q2
+
+// q3
+// let teamName = "Full Stack Developer...🧠";
+// alert(teamName);
+// q3
+
+// q4
+// var bestMan = "Charlie";
+// bestMan = "Zohaib";
+// q4
+
+// Chapter 3 (Variables for numbers)
+
+// q1
+// let caseQty;
+// q1
+
+// q2
+// caseQty = 144;
+// q2
+
+// q3
+// var num = 9;
+// q3
+// q4
+
+// let sum;
+// sum = 5 + 7;
+// q4
+
+// q5
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge; // ans is    = 110
+
+// q5
+// q6
+// let myNumber = 20;
+// myNumber = myNumber + 10; // ans is    30
+// q6
